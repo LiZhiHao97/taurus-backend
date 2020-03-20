5ee8bfc7b83df89f4d25171eabf85da358fcb7a0
lizhihao97 <940166841@qq.com>
2020-03-20 20:51:21 +0800

    perfect users module

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -8,7 +8,10 @@ class UsersController {
     }
 
     async findById (ctx) {
-        const user = await User.findById(ctx.params.id);
+        const { fields } = ctx.query;
+        console.log(fields);
+        const selectFields = fields.split(';').filter(f => f).map(f => ' +' + f).join('');
+        const user = await User.findById(ctx.params.id).select(selectFields);
         if (!user) {
             ctx.throw(404, '用户不存在');
         }
@@ -39,7 +42,13 @@ class UsersController {
     async update (ctx) {
         ctx.verifyParams({
             name: { type: 'string', required: false },
-            password: { type: 'string', required: false }
+            password: { type: 'string', required: false },
+            avatar_url: { type: 'string', required: false },
+            gender: { type: 'string', required: false },
+            headline: { type: 'string', required: false },
+            locations: { type: 'array', itemType: 'string', required: false },
+            educations: { type: 'array', itemType: 'object', required: false },
+            tags: { type: 'array', itemType: 'string', required: false }
         })
         const user = await User.findByIdAndUpdate(ctx.params.id, ctx.request.body, {new: true});
         if (!user) {
--- app/models/users.js
+++ app/models/users.js
@@ -5,8 +5,22 @@ const { Schema, model } = mongoose;
 const userSchema = new Schema({
     __v: {type: Number,select: false},
     name: { type: String, required: true },
-    password: { type: String, require: true, select: false }
-
+    password: { type: String, require: true, select: false },
+    avatar_url: { type: String },
+    gender: { type: String, enum: ['male', 'female'], default: 'male', required: true },
+    headline: { type: String },
+    locations: { type: [{type: String}], select: false },
+    educations: {
+        type: [{
+            school: { type: String },
+            major: { type: String },
+            diploma: { type: Number, enum: [1, 2, 3, 4, 5] },
+            entrance_year: { type: Number },
+            graduation_year: { type: Number }
+        }],
+        select: false
+    },
+    tags: { type: [{type: String}], select: false }
 });
 
 module.exports = model('User', userSchema); 

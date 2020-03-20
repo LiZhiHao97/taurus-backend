0ff9a4ad3d034ed026bb4ef602a8395bc6ca430b
lizhihao97 <940166841@qq.com>
2020-03-20 20:06:03 +0800

    add upload api

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,6 +1,14 @@
+const path = require('path');
+
 class HomeController {
     index(ctx) {
         ctx.body = '<h1>这是主页</h1>';
+    }
+
+    upload(ctx) {
+        const file = ctx.request.files.file;
+        const basename = path.basename(file.path);
+        ctx.body = { url: `${ctx.origin}/uploads/${basename}`};
     }
 }
 
--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
 const Koa = require('koa');
-const bodyparser = require('koa-bodyparser');
+const koaBody = require('koa-body');
+const koaStatic = require('koa-static');
 const error = require('koa-json-error');
 const parameter = require('koa-parameter'); 
+const path = require('path');
 const mongoose = require('mongoose');
 const app = new Koa();
 const routing = require('./routes');
@@ -10,11 +12,18 @@ const { connectionStr } = require('./config');
 mongoose.connect(connectionStr, { useUnifiedTopology: true, useFindAndModify: false, useNewUrlParser: true }, () => console.log('MongoDB 连接成功'));
 mongoose.connection.on('error', console.error);
 
+app.use(koaStatic(path.join(__dirname, 'public')));
 // 错误捕获
 app.use(error({
     postFormat: (e, {stack, ...rest})  => process.env.NODE_ENV  ===  'production' ? rest : {stack, ...rest}
 }));
-app.use(bodyparser());
+app.use(koaBody({
+    multipart: true, // 支持文件格式
+    formidable: {
+        uploadDir: path.join(__dirname, '/public/uploads'),
+        keepExtensions: true,
+    }
+}));
 // 参数校验
 app.use(parameter(app));
 routing(app);

3a5ebc1e3babf8097bff8bbcdb6eca3a1f1f71c0
lizhihao97 <940166841@qq.com>
2020-03-21 16:57:48 +0800

    add key search

--- app/controllers/labels.js
+++ app/controllers/labels.js
@@ -5,7 +5,7 @@ class LabelsController {
         const { per_page = 10 } = ctx.query;
         const page = Math.max(ctx.query.page * 1, 1) - 1;
         const perPage = Math.max(per_page * 1, 1);
-        ctx.body = await Label.find().limit(perPage).skip(page * perPage);
+        ctx.body = await Label.find({name: new RegExp(ctx.query.q)}).limit(perPage).skip(page * perPage);
     }
 
     async findById (ctx) {
